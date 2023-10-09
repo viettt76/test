3ccc008b21aad6bce53edff314adddf538cb8a08
viettt76 <viettt76@gmail.com>
2023-10-09 23:05:14 +0700

    first commit

---
+++ src/AppTest/AppTest.js
@@ -0,0 +1,17 @@
+
+
+function AppTest() {
+  return (
+    <div>
+      <iframe 
+        allowFullScreen
+        width={1000}
+        height={530}
+        title="john wick"
+        src="https://2embed.org/e.php?id=tt10366206"
+      />
+    </div>
+  );
+}
+
+export default AppTest;
---
+++ src/pages/News.js
@@ -0,0 +1,10 @@
+
+function News() {
+    return (
+        <div>
+            <h1>News page</h1>
+        </div>
+    )
+}
+
+export default News
